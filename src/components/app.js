b17a38ff253d01c8382e7829a1380b53e95d7c00
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-01-31 12:45:18 -0800

    initial game setup complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,12 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import Game from'./guess';
+
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+    <div className="container">
+        <h1 className="center-align">Guessing Game</h1>
+        <Game />
     </div>
 );
 
